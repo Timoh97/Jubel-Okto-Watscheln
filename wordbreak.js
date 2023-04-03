c7797750fae97b47fad898e0d80b8d2c9b4267cb
Timoh97 <timohmugendi@gmail.com>
2023-04-03 16:39:36 +0300

    word break

---
+++ wordbreak.js
@@ -0,0 +1,19 @@
+// Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+// Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+
+function wordBreak(s, wordDict) {
+    const dp = new Array(s.length + 1).fill(false);
+    dp[0] = true;
+    for (let i = 1; i <= s.length; i++) {
+      for (let j = 0; j < i; j++) {
+        if (dp[j] && wordDict.has(s.slice(j, i))) {
+          dp[i] = true;
+          break;
+        }
+      }
+    }
+    return dp[s.length];
+  }
+  
