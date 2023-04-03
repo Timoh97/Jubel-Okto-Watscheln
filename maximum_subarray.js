af56f12606a4781bb3a5dd72d3e8eecbac5aa1fa
Timoh97 <timohmugendi@gmail.com>
2023-04-03 15:50:09 +0300

    maximum sub array

---
+++ maximum_subarray.js
@@ -0,0 +1,12 @@
+// Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+function maxSubArray(nums) {
+    let maxSum = nums[0];
+    let currSum = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+      currSum = Math.max(nums[i], currSum + nums[i]);
+      maxSum = Math.max(maxSum, currSum);
+    }
+    return maxSum;
+  }
+  
